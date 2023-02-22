1ba3274f896e19962e8fa8cfdcddc07279f22f77
Mihai <mihaicosminrosu@yahoo.com>
2023-02-22 03:16:02 +0200

    Changed Rewind script name

--- Assets/Scripts/Rewind.cs
+++ Assets/Scripts/Rewind.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Rewind : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject level;
+    private GameObject save = null;
+    private void Awake()
     {
-        
+        level = GameObject.Find("Level");
+        if (level == null)
+            Debug.Log("set up an empty called \"Level\" and put everything in the scene besides PlayerPackage and RewindManager in it.");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Save: Instantiate a clone of the level, disable it and save it.
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("saved");
+            save = Instantiate(level);
+            save.SetActive(false);
+        }
+        // Rewind: Delete current level, enable the save and renew the save.
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            if (save == null)
+                Debug.Log("no save");
+            else
+            {
+                Destroy(level);
+                level = save;
+                level.SetActive(true);
+
+                save = Instantiate(level);
+                save.SetActive(false);
+            }
+        }
     }
 }
