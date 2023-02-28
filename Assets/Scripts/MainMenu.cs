52fc5dac511adbffc010a7f24643379857e0112f
Popa Bogdan-Gabriel(PxGluz) <badddog25@gmail.com>
2023-02-28 13:11:53 +0200

    fist + knife animations + main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button start, exit;
+    [SerializeField] private String sceneName;
+    
+    
+    private void StartScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void ExitScene()
+    {
+        // Maybe we need more things to do before quitting
+        Application.Quit();
+    }
+    
+    private void Start()
+    {
+        start.onClick.AddListener(StartScene);
+        exit.onClick.AddListener(ExitScene);
+    }
+}
