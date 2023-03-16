16738a7a9af4f67630f0af33e0c13d1aa1af19c2
Popa Bogdan-Gabriel(PxGluz) <badddog25@gmail.com>
2023-03-16 19:57:57 +0200

    Created Lobby Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,24 +5,9 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Button start, exit;
-    [SerializeField] private String sceneName;
-    
-    
-    private void StartScene()
-    {
-        SceneManager.LoadScene(sceneName);
-    }
 
-    private void ExitScene()
-    {
-        // Maybe we need more things to do before quitting
-        Application.Quit();
-    }
-    
     private void Start()
     {
-        start.onClick.AddListener(StartScene);
-        exit.onClick.AddListener(ExitScene);
+        // Time.timeScale = 0;
     }
 }
